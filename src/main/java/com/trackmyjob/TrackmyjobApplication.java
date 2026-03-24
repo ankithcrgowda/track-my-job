2cb407e66104039abf8e3447b4decfb73b303e15
Ankith C R <ankithcr13@gmail.com>
2026-03-24 11:33:08 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/trackmyjob/TrackmyjobApplication.java
@@ -0,0 +1,13 @@
+package com.trackmyjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackmyjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackmyjobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trackmyjob/TrackmyjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackmyjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackmyjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
